a8b0a7e800829b8c231ebcffe040d8876de0f0a1
asem waleed azmi alkhabbas <asem.waleed11@gmail.com>
2026-03-14 18:28:14 +0300

    Brevo Api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Vite prefetch (كما كان)
         Vite::prefetch(concurrency: 3);
+
+        // Macro لإرسال الإيميل عبر Brevo API
+        Mail::macro('brevo', function ($to, $subject, $html) {
+
+            return Http::withHeaders([
+                'api-key' => env('BREVO_API_KEY'),
+                'Content-Type' => 'application/json',
+            ])->post('https://api.brevo.com/v3/smtp/email', [
+
+                'sender' => [
+                    'email' => env('MAIL_FROM_ADDRESS'),
+                    'name'  => env('MAIL_FROM_NAME'),
+                ],
+
+                'to' => [
+                    ['email' => $to]
+                ],
+
+                'subject' => $subject,
+
+                'htmlContent' => $html,
+
+            ]);
+        });
     }
 }
