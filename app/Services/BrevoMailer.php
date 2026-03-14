c523c0764f8921b9e87176d2118c31033c5b62ac
asem waleed azmi alkhabbas <asem.waleed11@gmail.com>
2026-03-14 21:51:23 +0300

    Add brevocontroller

---
+++ app/Services/BrevoMailer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class BrevoMailer
+{
+    public static function send($to, $subject, $html)
+    {
+        Http::withHeaders([
+            'api-key' => env('BREVO_API_KEY'),
+            'Content-Type' => 'application/json'
+        ])->post('https://api.brevo.com/v3/smtp/email',[
+            'sender'=>[
+                'email'=>env('MAIL_FROM_ADDRESS'),
+                'name'=>env('MAIL_FROM_NAME')
+            ],
+            'to'=>[
+                ['email'=>$to]
+            ],
+            'subject'=>$subject,
+            'htmlContent'=>$html
+        ]);
+    }
+}
