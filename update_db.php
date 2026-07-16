56ea8ffe5d23ee04839bb34c08a58167b9e03b75
asem waleed azmi alkhabbas <asem.waleed11@gmail.com>
2026-07-16 20:16:32 +0300

    chore: remove obsolete database maintenance scripts and load test artifacts

--- update_db.php
+++
@@ -1,7 +0,0 @@
-$course = \App\Models\Course::find(13);
-if ($course) {
-    $course->name = 'تحدي تجربة المنصة 🏆';
-    $course->code = 'DEMO2026';
-    $course->save();
-    echo 'DB updated locally';
-}
